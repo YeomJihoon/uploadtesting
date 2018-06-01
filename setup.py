bda278c53cd2ac9abe69a6a939956205dde9513a
YeomJihoon <37164092+YeomJihoon@users.noreply.github.com>
2018-06-01 11:42:47 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "CHECKIT",
+    version = '1.0',
+    py_modules = ['checkit'],
+    author = 'HY Univ. OSS team A-1',
+    author_email = 'suspicions@naver.com',
+    url = 'https://github.com/OSS-A-1/SQLiteDB',
+    dsecription = '',
+    install_requires=['colorama'],
+)
