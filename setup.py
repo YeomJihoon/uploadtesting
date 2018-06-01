1ecda1e9e6b4bdcbb61450f7f227e5090bfd25db
YeomJihoon <37164092+YeomJihoon@users.noreply.github.com>
2018-06-01 12:07:50 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 from setuptools import setup
 
 setup(
-    name = "CHECKIT",
+    name = "UPLOADTESTING",
     version = '1.0',
-    py_modules = ['checkit'],
-    author = 'HY Univ. OSS team A-1',
-    author_email = 'suspicions@naver.com',
-    url = 'https://github.com/OSS-A-1/SQLiteDB',
-    dsecription = '',
-    install_requires=['colorama'],
+    py_modules = ['test'],
+    url = 'https://github.com/YeomJihoon/uploadtesting'
 )
+    
